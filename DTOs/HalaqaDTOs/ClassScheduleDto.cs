74ecdf322cac1282a6506b8019bbf0f42721913a
NehalKhalid <nehalkha92@gmail.com>
2025-07-16 14:17:47 +0200

    Halaq V2 (ClassSchedule)

---
+++ DTOs/HalaqaDTOs/ClassScheduleDto.cs
@@ -0,0 +1,15 @@
+﻿using MotqenIslamicLearningPlatform_API.Enums;
+
+namespace MotqenIslamicLearningPlatform_API.DTOs.HalaqaDTOs
+{
+    public class ClassScheduleDto
+    {
+        public DaysOfWeek Day { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string HalaqaName { get; set; }
+        public bool IsDeleted { get; set; }
+
+
+    }
+}
---
+++ DTOs/HalaqaDTOs/CreateClassScheduleDto.cs
@@ -0,0 +1,12 @@
+﻿using MotqenIslamicLearningPlatform_API.Enums;
+
+namespace MotqenIslamicLearningPlatform_API.DTOs.HalaqaDTOs
+{
+    public class CreateClassScheduleDto
+    {
+        public DaysOfWeek Day { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int HalaqaId { get; set; }
+    }
+}
