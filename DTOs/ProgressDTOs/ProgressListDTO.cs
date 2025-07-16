3f1a3a97fec59f3ed60d758abdc1f556cbb7cce3
7egz <mustafareda3210@gmail.com>
2025-07-16 12:32:07 +0300

    Worked On Progress Tracking Module

---
+++ DTOs/ProgressDTOs/ProgressListDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace MotqenIslamicLearningPlatform_API.DTOs.ProgressDTOs
+{
+    public class ProgressListDTO
+    {
+        public DateTime Date { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
+        public string Evaluation { get; set; } = string.Empty;
+        public string StudentName { get; set; } = string.Empty;
+        public string HalaqaName { get; set; } = string.Empty;
+        public string HalaqaSubject { get; set; } = string.Empty;
+
+        // Quran Progress Tracking Details
+        public int FromAyah { get; set; }
+        public int ToAyah { get; set; }
+        public int FromSurah { get; set; }
+        public int ToSurah { get; set; }
+
+        // Islamic Subjects Progress Tracking Details
+        public string Subject { get; set; } = string.Empty;
+        public int FromPage { get; set; }
+        public int ToPage { get; set; }
+        public string LessonName { get; set; } = string.Empty;
+
+
+    }
+}
